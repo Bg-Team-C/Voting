a8b65e0099d939134638df528d87029c23feadae
JMario1 <jdonmarie@gmail.com>
2022-04-20 13:52:21 +0100

    Feat: Add Teacher role and result handling

--- packages/hardhat/contracts/Voting.sol
+++ packages/hardhat/contracts/Voting.sol
@@ -9,6 +9,25 @@ contract Voting {
     uint256 vote;
   }
 
+  struct Candidate {
+    uint candidateId;
+    bytes32 name;
+    bytes32 position;
+  }
+  
+
+  bytes32 constant TEACHER_ROLE = keccak256("TEACHER_ACCESS");
+  bytes32 constant CHAIRMAN_ROLE = keccak256("CHAIRMAN_ACCESS");
+  bytes32 constant STUDENT_ROLE = keccak256("STUDENT_ACCESS");
+
+  mapping(address => bytes32) userRole;
+  //candidate id to vote gotten
+  mapping(uint => uint) private votesObtained;
+  mapping(uint => Candidate) public candidates;
+
+
+  uint private candidateCounter;
+
   // To check if election is active or not
   bool public isActive = false;
   bool public isEnded = false;
@@ -35,6 +54,43 @@ contract Voting {
     candidates[voteIndex].voteCount += 1;
   }
 
+  function setElectionCandidates(string[] memory names, string[] memory position) public 
+  onlyTeacher(msg.sender)
+  {
+    candidateCounter = 1; // should be intailized/reset in start vote 
+    for (uint256 i = 0; i < names.length; i++) {
+      Candidate storage candidate = candidates[candidateCounter];
+      candidate.candidateId = candidateCounter;
+      candidate.name = names[i];
+      candidate.position = position[i];
+      candidateCounter++;
+    }
+  }
+
+
+  function publishResult()
+    public
+    onlyChairmanOrTeacher
+    electionEnded
+    returns(
+      string[],
+      string[],
+      uint[]
+    )
+    {
+      string[] names = new string[](candidateCounter);
+      string[] positions = new string[](candidateCounter);
+      uint[] votes = new uint[](candidateCounter);
+
+      for (uint256 i = 0; i < candidateCounter; i++) {
+        names[i] = candidates[i].name;
+        positions = candidates[i].position;
+        votes = votesObtained[i];
+      }
+
+      return(names, positions, votes);
+    }
+
   // * MODIFIERS *
 
   modifier onlyValidCandidate(uint256 _candidateId) {
@@ -54,4 +110,25 @@ contract Voting {
     require(isActive, "Election has not begun!");
     _;
     }
+
+
+  modifier electionEnded(){
+    require(isEnded, "Election is still ongoing");
+    _;
+  }
+
+  modifier onlyTeacher(address user) {
+    require(userRole[user] == TEACHER_ROLE, "You don't have the required privilege");
+    _;
+  }
+
+  modifier onlyChairmanOrTeacher (address user) {
+    require(
+      userRole[user] == TEACHER_ROLE ||
+      userRole[user] == CHAIRMAN_ROLE ||
+      "You don't have the required privilege"
+    );
+    _;
+  }
+
 }
